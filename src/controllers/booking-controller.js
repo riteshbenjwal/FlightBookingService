65ae77d01aaae977c1d48cc5e7aba3f20010a413
riteshbenjwal <riteshbenjwal7@gmail.com>
2023-02-12 15:40:05 +0530

    added rabbitmq setup

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,27 +1,45 @@
 const { StatusCodes } = require("http-status-codes");
+const { REMINDER_BINDING_KEY } = require("../config/serverConfig");
 const { BookingService } = require("../services/index");
+
+const { createChannel, publishMessage } = require("../utils/messageQueue");
 
 const bookingService = new BookingService();
 
-const create = async (req, res) => {
-  try {
-    const response = await bookingService.createBooking(req.body);
+class BookingController {
+  async sendMessageToQueue(req, res) {
+    const channel = await createChannel();
+    publishMessage(
+      channel,
+      REMINDER_BINDING_KEY,
+      JSON.stringify({ message: "Success" })
+    );
     return res.status(StatusCodes.OK).json({
-      message: "Successfully completed booking",
+      message: "Successfully sent message to queue",
       success: true,
       err: {},
-      data: response,
-    });
-  } catch (error) {
-    return res.status(error.statusCode).json({
-      message: error.message,
-      success: false,
-      err: error.explanation,
       data: {},
     });
   }
-};
 
-module.exports = {
-  create,
-};
+  async create(req, res) {
+    try {
+      const response = await bookingService.createBooking(req.body);
+      return res.status(StatusCodes.OK).json({
+        message: "Successfully completed booking",
+        success: true,
+        err: {},
+        data: response,
+      });
+    } catch (error) {
+      return res.status(error.statusCode).json({
+        message: error.message,
+        success: false,
+        err: error.explanation,
+        data: {},
+      });
+    }
+  }
+}
+
+module.exports = BookingController;
